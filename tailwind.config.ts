f383ecb7c7b70a447ef9a411e8950e4457eabacb
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-02-05 11:11:50 +0200

    feat: added font-family in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-geist-mono)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
