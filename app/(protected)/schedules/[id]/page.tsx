bcc00e85f9531216ba76423851bc3d5050a55ba7
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-02-06 14:59:02 +0200

    feat: added a scrollInView for Scheduled weeks scroll

--- app/(protected)/schedules/[id]/page.tsx
+++ app/(protected)/schedules/[id]/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import ScheduleDetails from "@/components/ScheduleDetails";
-import { schedulesNos } from "@/data";
-import Link from "next/link";
+import ScrollWeeks from "@/components/ScrollWeeks";
 
 export default async function Page({
   params,
@@ -19,22 +18,7 @@ export default async function Page({
         <div className="h-full items-center py-16 px-2 rounded-t-[50px] bg-white md:p-6">
           <h3 className="text-lg font-medium">Weeks</h3>
           <div className="overflow-x-auto py-4">
-            <div className="flex items-center px-1 gap-4 w-[1000px]">
-              {schedulesNos.map((num) => (
-                <Link
-                  scroll={false}
-                  href={`/schedules/${num}`}
-                  key={num}
-                  className={`ring-4 flex shadow-md hover:shadow-xl transition delay-150 duration-300 ease-in-out justify-center items-center size-12 ${
-                    id === num.toString()
-                      ? "ring-pinklet-500 bg-pinklet-500 hover:bg-pinklet-600 text-white"
-                      : "ring-pinklet-500/10 hover:bg-pinklet-500 hover:text-white text-turquoise-900"
-                  } rounded-full`}
-                >
-                  {num}
-                </Link>
-              ))}
-            </div>
+            <ScrollWeeks id={id} />
           </div>
           <ScheduleDetails id={id} />
         </div>
