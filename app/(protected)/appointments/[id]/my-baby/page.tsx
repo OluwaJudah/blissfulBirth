795c0dee2a1b17b723b1bf5f6f2879daa7de4b16
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-03-07 06:40:10 +0200

    feat: added Mommy and Baby pages

---
+++ app/(protected)/appointments/[id]/my-baby/page.tsx
@@ -0,0 +1,12 @@
+import BabyReport from "@/components/(protected)/appointments/[id]/my-baby/BabyReport";
+import Fetus from "@/components/(protected)/appointments/[id]/my-baby/Fetus";
+
+export default function Page() {
+  return (
+    <>
+      <Fetus />
+      <BabyReport />
+    </>
+  );
+}
+  
---
+++ app/(protected)/appointments/[id]/my-body/page.tsx
@@ -0,0 +1,11 @@
+import Mommy from "@/components/(protected)/appointments/[id]/my-body/Mommy";
+import MommyReport from "@/components/(protected)/appointments/[id]/my-body/MommyReport";
+
+export default function Page() {
+  return (
+    <>
+      <Mommy />
+      <MommyReport />
+    </>
+  );
+}
