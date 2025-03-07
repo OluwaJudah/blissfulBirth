fc34190fdf3fbf5ef84868e9c8d3d197bd6f42d2
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-03-07 11:55:42 +0200

    feat: Tab navigation of appointment

--- components/(protected)/appointments/[id]/Tabs.tsx
+++ components/(protected)/appointments/[id]/Tabs.tsx
@@ -1,22 +1,48 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
 
 const Tabs = () => {
-  
+  const { id } = useParams<{ id: string }>();
+  const pathname = usePathname();
+  const baseUrl = `/appointments/${id}`;
+  const searchParams = useSearchParams();
+  const from = searchParams.get("from");
+  const tabs = [
+    {
+      name: "My Baby",
+      url: `${baseUrl}/my-baby${from === "home" && "?from=home"}`,
+      pathname: `${baseUrl}/my-baby`,
+      icon: "/baby_2.png",
+    },
+    {
+      name: "My Body",
+      url: `${baseUrl}/my-body${from === "home" && "?from=home"}`,
+      pathname: `${baseUrl}/my-body`,
+      icon: "/pregnant_4.png",
+    },
+  ];
+
   return (
     <div className="flex flex-col gap-[35px] px-3">
       <div className="flex justify-between space-x-5 w-full">
-        <div className="flex place-items-center basis-1/2 h-[80px] p-3 rounded-2xl bg-pinklet-200 shadow-2xl">
-          <div className="flex items-center">
-            <Image src="/baby_2.png" height={100} width={45} alt="Baby 2" />
-            <span>My Baby</span>
-          </div>
-        </div>
-        <div className="flex place-items-center basis-1/2 h-[80px] p-3 rounded-2xl bg-pinklet-100">
-          <div className="flex items-center">
-            <Image src="/pregnant_4.png" height={100} width={45} alt="Baby 2" />
-            <span>My Body</span>
-          </div>
-        </div>
+        {tabs.map((t) => (
+          <Link
+            key={t.name}
+            href={t.url}
+            className={`flex place-items-center basis-1/2 h-[80px] p-3 rounded-2xl ${
+              pathname === t.pathname
+                ? "bg-pinklet-200 shadow-2xl"
+                : "bg-pinklet-100"
+            }`}
+          >
+            <div className="flex items-center">
+              <Image src={t.icon} height={100} width={45} alt="Baby 2" />
+              <span>{t.name}</span>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
