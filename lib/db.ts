da9380649b5806133bfb8f7c4bc825f725cdfd43
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-03-27 14:30:41 +0200

    feat: added mongodb integration

--- lib/db.ts
+++ lib/db.ts
@@ -1,29 +1,42 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
-if (!uri) {
+const MONGODB_URI = process.env.MONGODB_URI || "";
+const NODE_ENV = process.env.NODE_ENV || "";
+const CLIENT = process.env.CLIENT || "";
+
+if (!MONGODB_URI) {
   throw new Error(
     "Please define the MONGODB_URI environment variable inside .env.local"
   );
 }
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+interface MongooseGlobal {
+  conn: mongoose.Connection | null;
+  promise: Promise<mongoose.Connection> | null;
+}
 
+// Prevent multiple connections in development (Hot Reloading issue)
 declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+  var mongooseGlobal: MongooseGlobal;
 }
 
-// Ensure the MongoDB client is reused in development mode
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+let cached = global.mongooseGlobal || { conn: null, promise: null };
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
+
+  if (!cached.conn) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: `${CLIENT}-${NODE_ENV}`, // Replace with your DB name
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose.connection);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default clientPromise;
+export default dbConnect;
