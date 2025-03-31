c6600bb4fc5377bdef302a3e382af6d5c9645077
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-03-31 12:12:45 +0200

    refactor: created one header for logged user nav header

--- app/(protected)/appointments/page.tsx
+++ app/(protected)/appointments/page.tsx
@@ -1,11 +1,14 @@
 import Body from "@/components/(protected)/appointments/Body";
-import Header from "@/components/(protected)/appointments/Header";
+import Header from "@/components/(protected)/Header";
 
 export default function HomePage() {
+  const title = "Appointments";
+  const imgUrl = "/calendar.svg";
+
   return (
     <div className="flex h-screen items-center">
       <main className="pb-[40px] md:rounded-3xl md:shadow-2xl bg-turquoise-50 h-full md:h-[700px] border md:border-gray-400/2 w-[400px] md:w-[350px] mx-auto overflow-scroll">
-        <Header />
+        <Header title={title} imgUrl={imgUrl} />
         <Body />
       </main>
     </div>
