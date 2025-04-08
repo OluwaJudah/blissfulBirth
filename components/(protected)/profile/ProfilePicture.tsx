6aea78e3b9c3b1721d00710a813dc26342b276a4
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-04-08 14:10:50 +0200

    refactor: updated Profile styling

--- components/(protected)/profile/ProfilePicture.tsx
+++ components/(protected)/profile/ProfilePicture.tsx
@@ -12,8 +12,8 @@ const ProfilePicture = () => {
           alt="User 1"
         />
       </div>
-      <div className="flex flex-col gap-1">
-        <span className="font-sans font-bold text-turquoise-900 text-lg leading-none">
+      <div className="flex flex-col gap-1 w-[200px]">
+        <span className="font-sans font-bold text-turquoise-900 text-lg leading-none truncate">
           Yanela Oluwaseeki
         </span>
         <p className="text-black text-sm leading-none">
