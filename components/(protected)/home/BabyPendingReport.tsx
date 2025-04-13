fed6d25146a28275d3ac5a3ce833f7021bcbee74
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-04-13 14:47:18 +0200

    refactor: update styling for home page

--- components/(protected)/home/BabyPendingReport.tsx
+++ components/(protected)/home/BabyPendingReport.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const BabyPendingReport = () => {
   return (
     <div className="embla__slide__1 h-[210px] md:w-[80px] w-[90px]">
-      <div className="relative embla__slide__number shadow-xl flex flex-col w-[315px] md:w-[295px] h-[180px] bg-turquoise-100 rounded-2xl pr-4 py-6 overflow-y-hidden">
+      <div className="relative embla__slide__number shadow-xl flex flex-col w-[310px] md:w-[295px] h-[180px] bg-turquoise-100 rounded-2xl pr-4 py-6 overflow-y-hidden">
         <div className="relative">
           <div className="flex flex-col h-full w-4/6 gap-1 float-right">
             <div className="text-center">
