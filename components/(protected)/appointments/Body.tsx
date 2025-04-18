0723dfb7faa7cfc45991f154caa9da7bf8b73875
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-04-18 11:34:14 +0200

    feat: added

--- components/(protected)/appointments/Body.tsx
+++ components/(protected)/appointments/Body.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import NextAppointment from "./NextAppointment";
 import MyBaby from "./MyBaby";
-import MyBody from "./MyBody";
 import DueDate from "./DueDate";
 
 const Body = () => {
   return (
-    <div className="px-[20px] flex flex-col gap-[35px]">
+    <div className="px-[20px] flex flex-col gap-[30px]">
       <NextAppointment />
       <MyBaby />
-      <MyBody />
       <DueDate />
     </div>
   );
