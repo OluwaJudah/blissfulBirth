4e3ab0dea8b20f7c34d97fe7ff55d9cf319c4d2b
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-05-06 11:11:20 +0200

    refactor: added featurre to add note and confirm booking

--- app/(protected)/confirmed-booking/page.tsx
+++ app/(protected)/confirmed-booking/page.tsx
@@ -8,13 +8,13 @@ export default async function HomePage({
   searchParams: SearchParams;
 }) {
   const query = await searchParams;
-  const { from } = query;
+  const { from, book } = query;
 
   return (
     <div className="flex h-screen items-center">
       <main className="md:rounded-3xl md:shadow-2xl bg-white h-full md:h-[750px] border md:border-gray-400/2 w-[400px] md:w-[350px] mx-auto overflow-y-hidden">
         <Header />
-        <Body />
+        <Body from={from} book={book} />
       </main>
     </div>
   );
--- components/(protected)/confirmed-booking/Body.tsx
+++ components/(protected)/confirmed-booking/Body.tsx
@@ -1,9 +1,10 @@
-import { Calendar, Calendar1, CalendarCheck, Home, Pen } from "lucide-react";
+import { Calendar1, CalendarCheck } from "lucide-react";
 import Location from "./Location";
 import TimeSlot from "./TimeSlot";
-import Link from "next/link";
+import NoteBackHome from "./NoteBackHome";
+import NotesAndBooking from "./NotesButton";
 
-const Body = () => {
+const Body = ({ book, from }: { book?: string; from?: string }) => {
   return (
     <>
       <div className="flex flex-col h-full gap-[10px] rounded-t-[50px] bg-turquoise-50 py-8 flex flex-col">
@@ -31,31 +32,7 @@ const Body = () => {
         <div className="border border-t-turquoise-100 my-3"></div>
         <TimeSlot />
         <Location />
-        <div className="flex flex-col gap-3 px-7">
-          <div className="flex items-center gap-2">
-            <div className="flex justify-center items-center bg-pinklet-200 w-[40px] h-[40px] rounded-full">
-              <Pen size={20} strokeWidth={3} />
-            </div>
-            <p className="font-mono font-bold">Notes</p>
-          </div>
-          <div className="w-full">
-            <p className="font-sans font-medium text-turquoise-950 text-base">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer...
-            </p>
-          </div>
-        </div>{" "}
-        <div className="border border-t-turquoise-100 my-3"></div>
-        <div className="w-full px-7">
-          <Link
-            className="flex items-center mx-auto justify-center gap-3 bg-turquoise-500 hover:bg-turquoise-700 text-white rounded-full w-full h-[35px]"
-            href="/home"
-          >
-            <Home size={15} color="#fff" />
-            <p className="text-center">Back to Home</p>
-          </Link>
-        </div>
+        {book ? <NotesAndBooking from={from} /> : <NoteBackHome from={from} />}
       </div>
     </>
   );
