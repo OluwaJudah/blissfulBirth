6dd03297282bf16638b4ad4acbc77df4bd0f64c9
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-05-19 22:36:05 +0200

    refactor: added not available payment entry on payment history

--- components/(protected)/profile/details/PaymentHistory.tsx
+++ components/(protected)/profile/details/PaymentHistory.tsx
@@ -3,14 +3,22 @@ import PaymentEntry from "./PaymentEntry";
 
 const PaymentHistory = async () => {
   const payments = await getPayments();
-  if (!payments) return null;
-
   return (
-    <div className="pt-4 pb-8 px-4 flex flex-col gap-3">
-      {payments.map((p, index) => (
-        <PaymentEntry {...p} key={p.id} />
-      ))}
-    </div>
+    <>
+      {payments && payments.length > 0 ? (
+        <div className="h-pt-4 pb-8 px-4 flex flex-col gap-3">
+          {payments.map((p, index) => (
+            <PaymentEntry {...p} key={p.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="h-1/2 pt-4 pb-8 px-4 flex flex-col gap-3">
+          <div className="h-full flex items-center justify-center">
+            No Payment Entry Available!!
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
