9247f8ab67d58e2c4d0f892d2b5714f75bc271e6
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-06-24 07:01:16 +0200

    refactor: updated the next generated tuesday

--- utils/index.ts
+++ utils/index.ts
@@ -95,7 +95,7 @@ export const getLastTuesdayFromCurrentOrNextMonth = () => {
   let lastTuesday = getLastTuesday(currentYear, currentMonth);
 
   // If it's before today, move to next month
-  if (lastTuesday > today) {
+  if (lastTuesday.getDate() !== today.getDate() && lastTuesday < today) {
     const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
     const nextYear = currentMonth === 11 ? currentYear + 1 : currentYear;
     lastTuesday = getLastTuesday(nextYear, nextMonth);
