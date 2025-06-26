86522cfece25d58065fa050ee7465046432e6f79
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-06-26 10:56:35 +0200

    refactor: added admin constant details

--- constants/user.ts
+++ constants/user.ts
@@ -1,2 +1,15 @@
 export const CLIENT = "client";
 export const CREDENTIALS = "credentials";
+const adminUser = {
+  username: "blissfuladmin.com",
+  password: "$2b$10$EC7lxhH3mfHotHgMs9x5aOtmUSsHj9BYGDNuCIDs7ICCPLJlnfUPa",
+  authType: "credentials",
+  role: "admin",
+  createdAt: {
+    $date: "2025-06-08T14:55:20.738Z",
+  },
+  updatedAt: {
+    $date: "2025-06-08T14:55:20.738Z",
+  },
+  __v: 0,
+};
