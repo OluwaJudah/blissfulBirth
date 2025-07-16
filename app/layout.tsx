b06d5b17e657397b91bb5eb3d40cc35b539809ac
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-07-16 14:37:40 +0200

    refactor: updated PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
