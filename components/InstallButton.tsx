59298dae31d2f705d65e00ef9a5917ca680a108c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-07-21 10:51:15 +0200

    refactor: updated pwa install button

--- components/InstallButton.tsx
+++ components/InstallButton.tsx
@@ -69,7 +69,7 @@ export default function InstallButton() {
 
   return (
     <>
-      {showButton && (
+      {!showButton && (
         <motion.div
           className="flex items-center justify-items-start p-0 m-0 mb-[20px] list-none cursor-pointer w-full"
           variants={itemVariants}
