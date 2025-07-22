e88a7d25ecb177437ad39d43b9e89801c27f11c3
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-07-22 11:33:43 +0200

    feat: registering SW manually

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PrelineScript from "@/components/PrelineScript";
+import RegisterSW from "@/components/RegisterSW";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -88,6 +89,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <RegisterSW />
       </body>
       <PrelineScript />
     </html>
